fbffcde1f17327da6d605e95b45914df90ef4f00
onokajames96 <jamesonoka996@gmail.com>
2023-10-14 08:21:56 -0700

    description

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AirBnB clone
+writing  a command line interpreter.
+This is the first step towards building your first full web application: the AirBnB clone.
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,3 +8,10 @@ from models.user import User
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+
+
+    def all(self):
+        return FileStora.__objects
+    def save(self):
+        with open("storage.json", "w") as file:
+            json.dump(self.objects, file)
