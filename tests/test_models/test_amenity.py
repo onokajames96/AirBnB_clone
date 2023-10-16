b4714bd2d53548f917fe0986ae54bd61c1b6d445
onokajames96 <jamesonoka996@gmail.com>
2023-10-16 09:11:11 -0700

    test case

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Testing Class Amenity"""
+import unittest
+from models.amenity import Amenity
+
 
 class TestAmenity(unittest.TestCase):
+    """Testing Amenity class"""
+    def test_amenity_name_default(self):
+        """Testing Amenity attributes"""
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
 
